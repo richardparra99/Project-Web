e8bb4fa9ff8e2d6eb81b8d5a4b8106f952730228
richardparra99 <richardparra.068@gmail.com>
2025-06-02 01:25:46 -0400

    agregando archivos al proyecto

--- js/paginaCuenta.js
+++ js/paginaCuenta.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Para Categoría
   const botonCategoria = document.getElementById('btn-categoria');
   const subcategorias = document.getElementById('subcategorias');
 
   if (botonCategoria && subcategorias) {
-    botonCategoria.addEventListener('click', () => {
+    botonCategoria.addEventListener('click', (e) => {
+      e.preventDefault(); // Evita que el <a href=""> recargue
       subcategorias.classList.toggle('oculto');
     });
   }
+
+  // Para Chats 👇
+  const botonChats = document.getElementById('btn-chats');
+  const subchats = document.getElementById('subchats');
+
+  if (botonChats && subchats) {
+    botonChats.addEventListener('click', (e) => {
+      e.preventDefault(); // Muy importante
+      subchats.classList.toggle('oculto');
+    });
+  }
 });
+
